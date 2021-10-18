79e262607a704ab52332169741a8708e63c0e6b3
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2021-10-18 14:55:56 -0300

    Finalização do Front-end na home

--- wp-content/themes/tipsramonoliveira/template-parts/banner.php
+++ wp-content/themes/tipsramonoliveira/template-parts/banner.php
@@ -1,12 +1,17 @@
 <section id="texto">
-    TEXTO AQUI
+    <h1>Bem vindo!</h1>
+    <h2>Aproveite meu acevo pessaol de anotações.</h2>
+    <p>Encontre dicas e tutorias diretos, rápidos e descomplicados sobre diversas tecnlogias. Esses são resultados de diversas anotações e resumos realizados em alguns anos estudando.</p>
+    <br>
+    <p><strong>Agora que você entendeu onde está, vamos começar...</strong></p>
+   
 </section>
 
 <section id="icons">
     <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\laptop.png" alt="laptop" class="laptop">
-    <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\CSS.png" alt="" class="css">
-    <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\HTML.png" alt="" class="html">
-    <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\javascript.png" alt="" class="javascript">
-    <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\python.png" alt="" class="python">
-    <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\react.png" alt="" class="react">
+    <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\CSS.png" alt="" class="circle_css">
+    <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\HTML.png" alt="" class="circle_html">
+    <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\javascript.png" alt="" class="circle_javascript">
+    <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\python.png" alt="" class="circle_python">
+    <img src="wp-content\themes\tipsramonoliveira\images\icones-banner\react.png" alt="" class="circle_react">
 </section>
